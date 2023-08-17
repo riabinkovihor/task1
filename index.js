122b2294fa0797df1c81f5aff0e940a8c9fd9d04
Ihor Riabinkov <i.riabinkov@freshdesign.ua>
2023-08-17 10:07:32 +0300

    Fixed error case

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 import params from './params.json' assert {type: 'json'}
 
+const state = {
+    inputObject: {},
+    outputObject: {},
+    error: ''
+}
+
 const $ = (selector) => {
     return document.querySelector(selector)
+}
+
+const convert = (value) => {
+    try {
+        state.inputObject = JSON.parse(value)
+        state.error = ''
+    } catch (e) {
+        if (value) state.error = 'Invalid JSON'
+        state.outputObject = {}
+        return
+    }
+
+    document.querySelector('#error').innerHTML = ''
+    const originalRation = Object.entries(params).find(item => {
+        return item[0] === state.inputObject.distance.unit.trim()
+    })?.[1]
+    const convertToRation = Object.entries(params).find(item => {
+        return item[0] === state.inputObject.convertTo.trim()
+    })?.[1]
+    const convertedValue =  state.inputObject.distance.value * originalRation / convertToRation
+
+    state.outputObject = {
+        unit: state.inputObject.convertTo,
+        value: convertedValue
+    }
 }
 
 const updateElValue = (el,value) => {
     if (el.innerHTML !== value) el.innerHTML = value
 }
-const state = {
-    inputObject: '',
-    outputObject: '',
-    error: ''
-}
+
 window.addEventListener('DOMContentLoaded', () => {
     const inputEl = $('#inputObject')
     const outputEl = $('#outputObject')
     const errorEl = $('#error')
 
     const handleChange = (value) => {
-        try {
-            state.inputObject = JSON.parse(value)
-
-        } catch (e) {
-            if (value) errorEl.innerHTML = 'Invalid JSON'
-            return
-        }
-        document.querySelector('#error').innerHTML = ''
-        const originalRation = Object.entries(params).find(item => {
-            return item[0] === state.inputObject.distance.unit.trim()
-        })?.[1]
-        const convertToRation = Object.entries(params).find(item => {
-            return item[0] === state.inputObject.convertTo.trim()
-        })?.[1]
-        const convertedValue =  state.inputObject.distance.value * originalRation / convertToRation
-
-        state.outputObject = {
-            unit: state.inputObject.convertTo,
-            value: convertedValue
-        }
+        convert(value)
 
         updateElValue(inputEl, JSON.stringify(state.inputObject))
         updateElValue(outputEl, JSON.stringify(state.outputObject))
